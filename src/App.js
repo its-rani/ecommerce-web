94422367efdf34b0cbd96b4e2d5f04ea243f8ea1
Rani Kumari <xranikumarix17@gmail.com>
2025-09-17 09:16:06 +0530

    front

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navigation } from '@mui/icons-material';
+import Navigations from './customer/components/Navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
-      hello 
+
+<Navigations/>
+      <div>
+        home
+      </div>
     </div>
   );
 }
